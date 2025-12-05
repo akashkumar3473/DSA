5ff933dcf105c207c58d597033a62518d9f45151
akashkumar3473@ <akashsingh34685@gmail.com>
2025-12-05 22:28:22 +0530

    Time: 55 ms (10.88%) | Memory: 46.7 MB (99.92%) - LeetSync

---
+++ 303-range-sum-query-immutable/range-sum-query-immutable.java
@@ -0,0 +1,14 @@
+class NumArray {
+    int[] arr;
+    public NumArray(int[] nums) {
+        arr = nums; // shallow copy
+    }
+    
+    public int sumRange(int left, int right) {
+        int sum = 0;
+        for(int i=left;i<=right;i++){
+            sum += arr[i];
+        }
+        return sum;
+    }
+}
