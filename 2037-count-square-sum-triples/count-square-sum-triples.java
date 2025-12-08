a071bf05617c4306d2ee3478e73904d4898367d1
akashkumar3473@ <akashsingh34685@gmail.com>
2025-12-08 22:16:07 +0530

    Time: 81 ms (19.00%) | Memory: 42.1 MB (37.58%) - LeetSync

--- 2037-count-square-sum-triples/count-square-sum-triples.java
+++ 2037-count-square-sum-triples/count-square-sum-triples.java
@@ -3,7 +3,7 @@ class Solution {
         int count =0;
         for(int i=1; i<n-1;i++){
             for(int j=i+1;j<n;j++){
-                for(int k=i+2;k<n+1;k++){
+                for(int k=i+2;k<=n;k++){
                     if(i*i+j*j==k*k) count++;
                 }
             }
