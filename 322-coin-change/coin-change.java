76e568996d52689963d5f5b7470f239f4ea9a84f
akashkumar3473@ <akashsingh34685@gmail.com>
2025-12-24 11:56:00 +0530

    Time: 16 ms (62.82%) | Memory: 46.2 MB (59.20%) - LeetSync

---
+++ 322-coin-change/coin-change.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+class Solution {
+    public int coinChange(int[] coins, int amount) {
+
+        if (amount == 0) return 0;
+
+        int[] minCoinsDP = new int[amount + 1];
+        Arrays.fill(minCoinsDP, Integer.MAX_VALUE);
+
+        minCoinsDP[0] = 0;
+
+        for (int i = 1; i <= amount; i++) {
+            for (int coin : coins) {
+                if (coin <= i && minCoinsDP[i - coin] != Integer.MAX_VALUE) {
+                    minCoinsDP[i] = Math.min(minCoinsDP[i],1 + minCoinsDP[i - coin]);
+                }
+            }
+        }
+
+        return minCoinsDP[amount] == Integer.MAX_VALUE ? -1 : minCoinsDP[amount];
+    }
+}
