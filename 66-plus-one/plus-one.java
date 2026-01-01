cac2ff2ee355dbd3a631683701df03aa2e44be3d
akashkumar3473@ <akashsingh34685@gmail.com>
2026-01-01 22:20:18 +0530

    Time: 0 ms (100.00%) | Memory: 43.6 MB (19.41%) - LeetSync

--- 66-plus-one/plus-one.java
+++ 66-plus-one/plus-one.java
@@ -1,60 +1,13 @@
 class Solution {
-
-    public int[]reverse(int[]arr){
-        int i = 0;
-        int j = arr.length-1;
-        while(i<j){
-            int temp = arr[i];
-            arr[i] = arr[j];
-            arr[j] = temp;
-            i++;
-            j--;
-        }
-        return arr;
-    }
     public int[] plusOne(int[] nums) {
-int []num = reverse(nums);
-int carry= 1;
-int sum = 0;
-String s = "";
-for(int i = 0;i<nums.length;i++){
-    if(i==nums.length-1){
-       sum = num[i]+carry;
-       carry = sum/10;
-       if(carry>=1){
-       s+=(char)(sum%10);
-       s+= (char)carry;}
-       else{
-s+=(char)(sum%10);
-       }
-    }
-
-    else{
- sum = num[i]+carry;
-       carry = sum/10;
-       s+=(char)(sum%10);
-    }
-}
-
-char[]ch = s.toCharArray(); 
-    int i = 0;
-    int j = ch.length-1;
-    while(i<j){
-        char temp = ch[i];
-        ch[i] = ch[j];
-        ch[j] = temp;
-        i++;
-        j--;
-    } 
-
-      
-      int []arr = new int[ch.length];
-
-      for(int p = 0;p<arr.length;p++){
-        arr[p] = (int)(ch[p]);
-
-      }
-      return arr;
-        
+        int n = nums.length;
+        for(int i=n-1;i>=0;i--){
+            nums[i]+=1;
+            if(nums[i]<10) return nums;
+            nums[i]=0;
+        }
+        nums = new int[n+1];
+        nums[0]=1;
+        return nums;
     }
 }
