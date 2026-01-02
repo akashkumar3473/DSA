ca5454c4efbf90ad23da6be33a4fd84f3b09d5ef
akashkumar3473@ <akashsingh34685@gmail.com>
2026-01-02 22:16:24 +0530

    Time: 6 ms (27.97%) | Memory: 46.1 MB (31.90%) - LeetSync

--- 9-palindrome-number/palindrome-number.java
+++ 9-palindrome-number/palindrome-number.java
@@ -1,15 +1,19 @@
 class Solution {
     public boolean isPalindrome(int x) {
-       String s = String.valueOf(x);
-        int i =0;
-        int j=s.length()-1;
-        while(i<j){
-            if(s.charAt(i)!=s.charAt(j)){
-                return false;
-            }
-            i++;
-            j--;
-        }
-        return true;
+        String s = String.valueOf(x);
+        int n = s.length();
+        int i=0;
+        int j =n-1;
+
+
+while(i<j){
+    if(s.charAt(i)!=s.charAt(j)) return false;
+
+    else{
+        i++;
+        j--;
+    }
+}
+  return true;     
     }
 }
