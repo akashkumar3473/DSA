5f266e8bec21c329654a7b9f8ee759dc5b6ff141
akashkumar3473@ <akashsingh34685@gmail.com>
2026-02-17 09:15:27 +0530

    Time: 3 ms (87.50%) | Memory: 52.4 MB (46.21%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -1,57 +1,18 @@
 import java.util.*;
 
 class Solution {
-
-    class Pair {
-        int row;
-        int col;
-
-        Pair(int row, int col) {
-            this.row = row;
-            this.col = col;
-        }
+    private void dfs(int i, int j, char[][] grid, boolean[][] vis){
+        int m = grid.length, n = grid[0].length;
+        vis[i][j] = true;
+        if(i-1>=0 && grid[i-1][j]=='1' && !vis[i-1][j])
+            dfs(i-1,j,grid,vis);
+        if(i+1<=m-1 && grid[i+1][j]=='1' && !vis[i+1][j])
+            dfs(i+1,j,grid,vis);
+        if(j-1>=0 && grid[i][j-1]=='1' && !vis[i][j-1])
+            dfs(i,j-1,grid,vis);
+        if(j+1<=n-1 && grid[i][j+1]=='1' && !vis[i][j+1])
+            dfs(i,j+1,grid,vis);
     }
-
-    private void bfs(int row, int col, char[][] grid, boolean[][] vis) {
-        int m = grid.length;
-        int n = grid[0].length;
-
-        Queue<Pair> q = new LinkedList<>();
-        q.add(new Pair(row, col));
-        vis[row][col] = true;   // mark starting cell visited
-
-        while (!q.isEmpty()) {
-
-            Pair front = q.remove();
-            int r = front.row;
-            int c = front.col;
-
-            // top
-            if (r - 1 >= 0 && !vis[r - 1][c] && grid[r - 1][c] == '1') {
-                q.add(new Pair(r - 1, c));
-                vis[r - 1][c] = true;
-            }
-
-            // down
-            if (r + 1 < m && !vis[r + 1][c] && grid[r + 1][c] == '1') {
-                q.add(new Pair(r + 1, c));
-                vis[r + 1][c] = true;
-            }
-
-            // left
-            if (c - 1 >= 0 && !vis[r][c - 1] && grid[r][c - 1] == '1') {
-                q.add(new Pair(r, c - 1));
-                vis[r][c - 1] = true;
-            }
-
-            // right
-            if (c + 1 < n && !vis[r][c + 1] && grid[r][c + 1] == '1') {
-                q.add(new Pair(r, c + 1));
-                vis[r][c + 1] = true;
-            }
-        }
-    }
-
     public int numIslands(char[][] grid) {
         int m = grid.length;
         int n = grid[0].length;
@@ -62,7 +23,7 @@ class Solution {
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 if (grid[i][j] == '1' && !vis[i][j]) {
-                    bfs(i, j, grid, vis);
+                    dfs(i, j, grid, vis);
                     count++;
                 }
             }
