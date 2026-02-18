a9e716b0ea7cd91106946074a9f2ebe1baec00c3
akashkumar3473@ <akashsingh34685@gmail.com>
2026-02-18 11:54:50 +0530

    Time: 506 ms (70.30%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1837-daily-leads-and-partners/daily-leads-and-partners.sql
@@ -0,0 +1,4 @@
+# Write your MySQL query statement below
+select date_id,make_name,count(distinct lead_id) As unique_leads, count(distinct partner_id) as unique_partners
+from  DailySales
+Group by date_id,make_name;
