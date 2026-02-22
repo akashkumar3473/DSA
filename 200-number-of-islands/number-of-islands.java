1197bd566b53a6aec1546220599580e9ee21a00b
akashkumar3473@ <akashsingh34685@gmail.com>
2026-02-22 17:06:11 +0530

    Time: 6 ms (23.01%) | Memory: 51.4 MB (88.24%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -1,34 +1,56 @@
-import java.util.*;
-
 class Solution {
-    private void dfs(int i, int j, char[][] grid, boolean[][] vis){
-        int m = grid.length, n = grid[0].length;
-        vis[i][j] = true;
-        if(i-1>=0 && grid[i-1][j]=='1' && !vis[i-1][j])
-            dfs(i-1,j,grid,vis);
-        if(i+1<=m-1 && grid[i+1][j]=='1' && !vis[i+1][j])
-            dfs(i+1,j,grid,vis);
-        if(j-1>=0 && grid[i][j-1]=='1' && !vis[i][j-1])
-            dfs(i,j-1,grid,vis);
-        if(j+1<=n-1 && grid[i][j+1]=='1' && !vis[i][j+1])
-            dfs(i,j+1,grid,vis);
+    class Pair{
+        int row;
+        int col;
+        Pair(int row,int col){
+            this.row = row;
+            this.col = col;
+        }
+    }
+    int n;
+    int m;
+    int[] dx = {-1,1,0,0};
+    int[] dy = {0,0,1,-1};
+    public boolean isValid(int r, int c, int n, int m){
+
+        if(r>=0 && r<n && c>=0 && c<m) return true;
+        return false;
+    }
+    public void bfs(char[][] grid,boolean[][] isVisited,int r,int c){
+        isVisited[r][c] = true;
+        Queue<Pair> q = new LinkedList<>();
+        q.add(new Pair(r,c));
+        while(!q.isEmpty()){
+            Pair p = q.poll();
+            int i=p.row;
+            int j =p.col;
+            for(int k=0;k<4;k++){
+                int adjrow = i + dx[k];
+                int adjcol = j + dy[k];
+                if(isValid(adjrow,adjcol,n,m)
+                && grid[adjrow][adjcol]=='1'
+                && !isVisited[adjrow][adjcol]){
+                    
+                    isVisited[adjrow][adjcol]=true;
+                    q.add(new Pair(adjrow,adjcol));
+                }
+                
+            }
+        }
     }
     public int numIslands(char[][] grid) {
-        int m = grid.length;
-        int n = grid[0].length;
-
-        boolean[][] vis = new boolean[m][n];
-        int count = 0;
-
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (grid[i][j] == '1' && !vis[i][j]) {
-                    dfs(i, j, grid, vis);
+        n = grid.length;
+        m = grid[0].length;
+        int count =0;
+        boolean[][] isVisited = new boolean[n][m];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j]=='1' && isVisited[i][j]==false){
+                    bfs(grid,isVisited,i,j);
                     count++;
                 }
             }
         }
-
         return count;
     }
 }
