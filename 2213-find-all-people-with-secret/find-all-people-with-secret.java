4e92197f68db4ef62f94fcacb500e646ea3f54c1
akashkumar3473@ <akashsingh34685@gmail.com>
2026-03-11 16:44:45 +0530

    Time: 73 ms (84.35%) | Memory: 283.7 MB (23.92%) - LeetSync

--- 2213-find-all-people-with-secret/find-all-people-with-secret.java
+++ 2213-find-all-people-with-secret/find-all-people-with-secret.java
@@ -1,50 +1,79 @@
 class Solution {
+
+    class DSU{
+        int[] parent;
+
+        DSU(int n){
+            parent = new int[n];
+            for(int i=0;i<n;i++) parent[i]=i;
+        }
+
+        int find(int x){
+            if(parent[x]!=x){
+                parent[x]=find(parent[x]);
+            }
+            return parent[x];
+        }
+
+        void union(int x, int y){
+            int px = find(x);
+            int py = find(y);
+            if(px!=py) parent[py] = px;
+        }
+
+        void reset(int x){
+            parent[x] = x;
+        }
+    }
+
     public List<Integer> findAllPeople(int n, int[][] meetings, int firstPerson) {
-        Arrays.sort(meetings, (a, b) -> a[2] - b[2]);
 
-        int[] parent = new int[n];
-        boolean[] know = new boolean[n];
-        for (int i = 0; i < n; i++) parent[i] = i;
+        Arrays.sort(meetings,(a,b)->a[2]-b[2]);
 
-        know[0] = know[firstPerson] = true;
+        DSU dsu = new DSU(n);
 
-        for (int i = 0; i < meetings.length; ) {
-            int t = meetings[i][2];
-            List<Integer> list = new ArrayList<>();
+        dsu.union(0,firstPerson);
+
+        int i=0;
+
+        while(i < meetings.length){
+
+            int time = meetings[i][2];
+
+            List<Integer> people = new ArrayList<>();
 
             int j = i;
-            while (j < meetings.length && meetings[j][2] == t) {
-                union(meetings[j][0], meetings[j][1], parent);
-                list.add(meetings[j][0]);
-                list.add(meetings[j][1]);
+
+            while(j < meetings.length && meetings[j][2] == time){
+
+                int x = meetings[j][0];
+                int y = meetings[j][1];
+
+                dsu.union(x,y);
+
+                people.add(x);
+                people.add(y);
+
                 j++;
             }
 
-            for (int p : list)
-                if (know[p])
-                    know[find(p, parent)] = true;
-
-            for (int p : list)
-                know[p] |= know[find(p, parent)];
-
-            for (int p : list)
-                parent[p] = p;
+            for(int p : people){
+                if(dsu.find(p) != dsu.find(0)){
+                    dsu.reset(p);
+                }
+            }
 
             i = j;
         }
 
-        List<Integer> res = new ArrayList<>();
-        for (int i = 0; i < n; i++)
-            if (know[i]) res.add(i);
-        return res;
-    }
+        List<Integer> ans = new ArrayList<>();
 
-    private int find(int x, int[] p) {
-        return p[x] == x ? x : (p[x] = find(p[x], p));
-    }
+        for(int k=0;k<n;k++){
+            if(dsu.find(k) == dsu.find(0)){
+                ans.add(k);
+            }
+        }
 
-    private void union(int a, int b, int[] p) {
-        int pa = find(a, p), pb = find(b, p);
-        if (pa != pb) p[pb] = pa;
+        return ans;
     }
 }
