3ca20be6c31f352a4ffe21c3d80ea465004ee4b1
akashkumar3473@ <akashsingh34685@gmail.com>
2026-03-12 12:16:43 +0530

    Time: 2 ms (45.36%) | Memory: 47.2 MB (85.27%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.java
+++ 547-number-of-provinces/number-of-provinces.java
@@ -1,37 +1,41 @@
 class Solution {
-    // public void bfs(int i,boolean[] vis,int[][] adj){
-    //     int n= adj.length;
-    //     vis[i]=true;
-    //     Queue<Integer> q = new LinkedList<>();
-    //     q.add(i);
-    //     while(q.size()>0){
-    //         int front = q.remove();
-    //         for(int j=0;j<n;j++){
-    //             if(adj[front][j]==1 && !vis[j]){
-    //                 q.add(j);
-    //                 vis[j]=true;
-    //             }
-    //         }
-    //     }
-    // }
-    public void dfs(int i,boolean[] vis,int[][] adj){
-        int n= adj.length;
-        vis[i]=true;
-        for(int j=0;j<n;j++){
-            if(adj[i][j]==1 && !vis[j]){
-                dfs(j,vis,adj);
+    static int[] parent;
+    static int[] size;
+    public int find(int a){
+        if(parent[a]==a) return a;
+        return find(parent[a]);
+    }
+    public void union(int a, int b){
+        a = find(a);
+        b = find(b);
+        if(a!=b){
+            if(size[a]>size[b]){  // a should be parent of b
+                parent[b] = a;
+                size[a] += size[b];
             }
+            else{
+                parent[a] = b;
+                size[b] += size[a];
+            }   
+                
         }
     }
-    public int findCircleNum(int[][] adj) {
-        int n = adj.length;
-        int count =0;
-        boolean[] vis = new  boolean[n];
+    public int findCircleNum(int[][] arr) {
+        int n = arr.length;
+        parent = new int[n+1];  // 1 to n nodes
+        size = new int[n+1];
+        for(int i=1;i<=n;i++){
+            parent[i] = i; 
+            size[i]=1;
+        }
         for(int i=0;i<n;i++){
-            if(!vis[i]){
-                dfs(i,vis,adj);
-                count++;
+            for(int j=0;j<n;j++){
+                if(i!=j && arr[i][j]==1) union(i+1,j+1);
             }
+        }
+        int count = 0;
+        for(int i=1;i<=n;i++){
+            if(parent[i]==i) count++;
         }
         return count;
     }
