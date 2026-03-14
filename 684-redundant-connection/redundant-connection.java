0c84422f73d51b78b9c216aece43ae2c79feca67
akashkumar3473@ <akashsingh34685@gmail.com>
2026-03-14 09:35:43 +0530

    Time: 1 ms (82.00%) | Memory: 45 MB (33.00%) - LeetSync

--- 684-redundant-connection/redundant-connection.java
+++ 684-redundant-connection/redundant-connection.java
@@ -1,40 +1,40 @@
 class Solution {
     static int[] parent;
     static int[] size;
-    int leader( int a){
-        if(parent[a]==a) return a;
-        return parent[a] = leader(parent[a]);
+    int find(int u){
+        if(parent[u]==u) return u;
+        return parent[u] = find(parent[u]);
     }
-    void union(int a,int b){
-        a = leader(a);
-        b = leader(b);
+    void union(int u,int v){
+        int a = find(u);
+        int b = find(v);
         if(a!=b){
             if(size[a]>size[b]){
-                parent[b]=a;
-                size[a] -= size[b];
+                parent[b] = a;
+                size[a] += size[b];
+
             }
-            else{
-                parent[a]=b;
-                size[b]+=a;
-            }
+            parent[a]=b;
+            size[b]=size[a];
         }
     }
     public int[] findRedundantConnection(int[][] edges) {
         int n = edges.length;
         parent = new int[n+1];
-        size = new  int[n+1];
-        for(int i=1;i<=n;i++){
+        size = new int[n+1];
+        for(int i=0; i<=n;i++){
             parent[i]=i;
-            size[i]=1;
+            size[i] = 1;
         }
         int[] ans = new int[2];
-        for(int[] arr:edges){
-            int a = arr[0] , b = arr[1];
-            if(leader(a)==leader(b)){
-                ans[0] = a; ans[1]=b; break;
+        for(int[] arr : edges){
+            int u = arr[0], v = arr[1];
+            if(find(u)==find(v)){
+                ans[0] = u ;ans[1] = v ; break;
             }
-            else union (a,b);
+            union(u,v);
         }
         return ans;
+
     }
 }
