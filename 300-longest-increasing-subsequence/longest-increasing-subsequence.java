786d29af3d4b20cf23c8ce12c8b44454dfad9877
akashkumar3473@ <akashsingh34685@gmail.com>
2026-03-16 11:25:56 +0530

    Time: 366 ms (13.03%) | Memory: 248.8 MB (6.12%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -1,30 +1,16 @@
 class Solution {
-    int[][] dp;
-
-    public int solve(int[] nums, int i, int n, int prev) {
-        if (i >= n) return 0;
-
-        if (dp[i][prev + 1] != -1)
-            return dp[i][prev + 1];
-
-        int take = 0;
-        if (prev == -1 || nums[i] > nums[prev]) {
-            take = 1 + solve(nums, i + 1, n, i);
-        }
-
-        int skip = solve(nums, i + 1, n, prev);
-
-        return dp[i][prev + 1] = Math.max(take, skip);
+    static int dp[][];
+    public int solve(int i,int prev,int[] nums){
+        if(i>=nums.length)return 0;
+        if(dp[i][prev+1]!=-1)return dp[i][prev+1];
+        int pick=0,nopick=solve(i+1,prev,nums);
+        if(prev==-1||nums[prev]<nums[i])pick=1+solve(i+1,i,nums);
+        return dp[i][prev+1]=Math.max(pick,nopick);
     }
-
     public int lengthOfLIS(int[] nums) {
-        int n = nums.length;
-        dp = new int[n][n + 1];   // changed from [n][2]
-
-        for (int i = 0; i < n; i++) {
-            Arrays.fill(dp[i], -1);
-        }
-
-        return solve(nums, 0, n, -1);  
+        int n  = nums.length;
+        dp=new int[n+1][n+1];
+        for(int[] r:dp)Arrays.fill(r,-1);
+        return solve(0,-1,nums);
     }
 }
