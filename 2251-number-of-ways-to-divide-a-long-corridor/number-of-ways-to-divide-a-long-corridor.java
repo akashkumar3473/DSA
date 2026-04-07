8da1dffab1c3836f01b538407e8f9d33b494ba0a
akashkumar3473@ <akashsingh34685@gmail.com>
2026-04-07 21:04:38 +0530

    Time: 34 ms (27.35%) | Memory: 47.5 MB (98.21%) - LeetSync

--- 2251-number-of-ways-to-divide-a-long-corridor/number-of-ways-to-divide-a-long-corridor.java
+++ 2251-number-of-ways-to-divide-a-long-corridor/number-of-ways-to-divide-a-long-corridor.java
@@ -1,33 +1,35 @@
 class Solution {
-    int mod = 1000000007;
-    long[][] dp;
-    long solve(String s, int i, int seats){
-        if(i==s.length()){
-            return seats==2?1:0;
+    public int numberOfWays(String corridor) {
+        long MOD = 1_000_000_007;
+        int totalSeats = 0;
+
+        for (char c : corridor.toCharArray()) {
+            if (c == 'S') totalSeats++;
         }
-        if(dp[i][seats]!=-1) return dp[i][seats];
-        char ch = s.charAt(i);
-        long opt1 = 0, opt2 = 0;
-        if(ch=='S'){
-            if(seats==2){
-                opt1 = solve(s,i+1,1);
-            }else{
-                opt1 = solve(s,i+1,seats+1);
+
+        if (totalSeats == 0 || totalSeats % 2 != 0) return 0;
+
+        long ways = 1;
+        int seatCount = 0;
+        int plants = 0;
+
+        for (char c : corridor.toCharArray()) {
+            if (c == 'S') {
+                seatCount++;
+
+                
+                if (seatCount > 2 && seatCount % 2 == 1) {
+                    ways = (ways * (plants + 1)) % MOD;
+                    plants = 0;
+                }
+            } else {
+                // Count plants only between segments
+                if (seatCount >= 2 && seatCount % 2 == 0) {
+                    plants++;
+                }
             }
         }
-        else{
-            opt1 = solve(s,i+1,seats);
-            if(seats==2)
-                opt2 = solve(s,i+1,0);
-        }
-        return dp[i][seats] = (opt1+opt2)%mod;
-    }
-    public int numberOfWays(String c) {
-        int n = c.length();
-        dp = new long[n][3];
-        for(int i=0;i<n;i++){
-            Arrays.fill(dp[i],-1);
-        }
-        return (int)solve(c,0,0);
+
+        return (int) ways;
     }
 }
