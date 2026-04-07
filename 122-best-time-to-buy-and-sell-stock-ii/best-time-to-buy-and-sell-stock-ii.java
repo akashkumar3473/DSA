2ba91f0084fd082227638f1e43869e8e7cd9ad58
akashkumar3473@ <akashsingh34685@gmail.com>
2026-04-07 21:13:54 +0530

    Time: 4 ms (8.47%) | Memory: 50 MB (6.30%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.java
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.java
@@ -4,13 +4,13 @@ class Solution {
         if(i==n) return 0;
         if(dp[i][buy]!=-1) return dp[i][buy];
         int profit = 0;
-        if(buy==0){
-            int take = -p[i]+solve(i+1,1,p,n);
-            int skip = solve(i+1,0,p,n);
+        if(buy==1){
+            int take = -p[i]+solve(i+1,0,p,n);
+            int skip = solve(i+1,1,p,n);
             profit = Math.max(take,skip);
         }else{
-            int sell = p[i] + solve(i+1,0,p,n);
-            int skip = solve(i+1,1,p,n);
+            int sell = p[i] + solve(i+1,1,p,n);
+            int skip = solve(i+1,0,p,n);
             profit = Math.max(sell,skip);
             
         }
@@ -22,6 +22,6 @@ class Solution {
         for(int i=0;i<n;i++){
             Arrays.fill(dp[i],-1);
         }
-        return solve(0,0,p,n);
+        return solve(0,1,p,n);
     }
 }
