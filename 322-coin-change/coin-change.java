0df08ef6f87936c2608ddcdf986f51cf35da28dd
akashkumar3473@ <akashsingh34685@gmail.com>
2026-04-18 10:23:44 +0530

    Time: 31 ms (18.55%) | Memory: 47.1 MB (25.37%) - LeetSync

--- 322-coin-change/coin-change.java
+++ 322-coin-change/coin-change.java
@@ -1,30 +1,23 @@
 class Solution {
-    int[] dp;
-    public int solve(int[] coins, int amount) {
-        // Base cases
-        if (amount == 0) return 0;
-        if (amount < 0) return Integer.MAX_VALUE;
-
-        // Memo check
-        if (dp[amount] != -1) return dp[amount];
-
-        int minCoins = Integer.MAX_VALUE;
-
-        // Try each coin
-        for (int coin : coins) {
-            int res = solve(coins, amount - coin);
-            if (res != Integer.MAX_VALUE) {
-                minCoins = Math.min(minCoins, res + 1);
-            }
+    Integer[][] dp;
+    public int solve(int idx,int[] coins,int t){
+        if(idx==0){
+            if(t%coins[idx]==0) return t/coins[idx];
+            else return (int)1e9;
         }
+        if(dp[idx][t]!=null) return dp[idx][t];
+        int skip = solve(idx-1,coins,t);
 
-        return dp[amount] = minCoins;
+        int take = (int)1e9;
+        if(coins[idx]<=t){
+            take = 1 + solve(idx,coins,t-coins[idx]);
+        }
+        return dp[idx][t] = Math.min(take,skip);
     }
-    public int coinChange(int[] coins, int amount) {
-        dp = new int[amount + 1];
-        Arrays.fill(dp, -1);
-
-        int ans = solve(coins, amount);
-        return ans == Integer.MAX_VALUE ? -1 : ans;
+    public int coinChange(int[] coins, int t) {
+        int n = coins.length;
+        dp = new Integer[n][t+1];
+        int ans = solve(n-1,coins,t);
+        return ans >= (int)1e9 ? -1 : ans;
     }
 }
