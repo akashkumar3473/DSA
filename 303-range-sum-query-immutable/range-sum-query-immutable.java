90856eb569ea0b708368eb4e198266aba5f05e78
akashkumar3473@ <akashsingh34685@gmail.com>
2026-04-20 14:21:35 +0530

    Time: 11 ms (17.30%) | Memory: 50.2 MB (5.72%) - LeetSync

--- 303-range-sum-query-immutable/range-sum-query-immutable.java
+++ 303-range-sum-query-immutable/range-sum-query-immutable.java
@@ -1,47 +1,36 @@
 class NumArray {
     int[] tree;
     int n;
-
     public NumArray(int[] nums) {
         n = nums.length;
-        tree = new int[4 * n]; // IMPORTANT: initialize tree
-        build(nums, 0, n - 1, 0);
+        tree = new int[4 * n];
+        build(nums,0,n-1,0);
     }
-
-    private void build(int[] nums, int lo, int hi, int node) {
+     private void build(int[] nums, int lo, int hi, int node) {
         if (lo == hi) {
             tree[node] = nums[lo];
             return;
         }
-
-        int mid = (lo + hi) / 2;
-
-        build(nums, lo, mid, 2 * node + 1);
-        build(nums, mid + 1, hi, 2 * node + 2);
-
-        tree[node] = tree[2 * node + 1] + tree[2 * node + 2];
+        int mid = (lo+hi)/2;
+        build(nums,lo,mid,2*node+1);
+        build(nums,mid+1,hi,2*node+2);
+        tree[node] = tree[2*node+1] + tree[2*node+2];
     }
 
+    
     public int sumRange(int left, int right) {
-        return query(0, n - 1, left, right, 0);
+        return query(0,n-1,left,right,0);
     }
-
-    public int query(int lo, int hi, int L, int R, int node) {
-        // No overlap
-        if (hi < L || lo > R) {
+    public int query(int lo, int hi ,int L,int R, int node){
+        if(hi<L || lo>R){
             return 0;
         }
-
-        // Complete overlap
-        if (lo >= L && hi <= R) {
+        if(lo>=L && hi<=R){
             return tree[node];
         }
-
-        int mid = (lo + hi) / 2;
-
-        int leftSum = query(lo, mid, L, R, 2 * node + 1);
-        int rightSum = query(mid + 1, hi, L, R, 2 * node + 2);
-
-        return leftSum + rightSum;
+        int mid = (lo+hi)/2;
+        int leftsum = query(lo,mid,L,R,2*node+1);
+        int rightsum = query(mid+1,hi,L,R,2*node+2);
+        return leftsum + rightsum;
     }
 }
